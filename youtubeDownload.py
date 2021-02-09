a883497069e512e248f246d8ecffd132e7c651ae
Krish Arora <krisharora27@gmail.com>
2021-02-09 11:11:43 -0500

    Worked on audio downloads and optimized code

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -5,7 +5,9 @@ from pytube import YouTube
 from tkinter import filedialog
 from tkinter import *
 from PIL import Image, ImageTk
+from moviepy.editor import *
 import urllib.request
+import os
 class Application(Frame):
 
 
@@ -18,12 +20,23 @@ class Application(Frame):
     def browse(self):
         dir = filedialog.askdirectory()
         self.path_ent.delete(0,"end")
-        self.path_ent.insert(0, str(dir)) 
-        print(self.path_ent.get())
+        self.path_ent.insert(0, str(dir))
 
     def downloadVid(self):
         self.pickList[self.chosenRes].download(self.path_ent.get())  # Path where to store the video
         self.submitButton['text'] = "Download a new video"
+
+    def downloadAud(self):
+        self.pickList[self.chosenRes].download(self.path_ent.get())
+
+        # mp4FileName = self.youtube.title + ".mp4"
+        # mp3FileName = self.youtube.title + ".mp3"
+
+        # videoClip = VideoFileClip(mp4FileName)
+        # audioClip = VideoClip.audio
+        # audioClip.write_audiofile(mp3FileName)
+        # audioClip.close()
+        # videoClip.close()
 
     def chooseResVid(self):
         for b in self.reslist:
@@ -31,8 +44,8 @@ class Application(Frame):
 
         self.chosenRes = 0
         video_url = str(self.url.get())
-        youtube = YouTube(video_url)
-        vidOptions = youtube.streams.filter(progressive=True, type='video', subtype='mp4').all()
+        self.youtube = YouTube(video_url)
+        vidOptions = self.youtube.streams.filter(progressive=True, type='video', subtype='mp4').all()
 
         resOptions = []
         self.pickList = []
@@ -51,39 +64,30 @@ class Application(Frame):
         self.submitButton = Button(self, text = "Next", command = self.downloadVid)
         self.submitButton.grid(row = row, column = 0, sticky=W)
         self.reslist.append(self.submitButton)
+
     def chooseResAudio(self):
         for b in self.reslist:
             b.destroy()
 
         self.chosenRes = 0
         video_url = str(self.url.get())
-        youtube = YouTube(video_url)
-        audOptions = youtube.streams.filter(type='audio').all()
+        self.youtube = YouTube(video_url)
+        audOptions = self.youtube.streams.filter(type='audio', subtype='mp4').all()
 
-        resOptions = []
         self.pickList = []
         for x in audOptions:
-            if x.abr not in resOptions:
-                resOptions.append(x.abr)
-                self.pickList.append(x)
-
-        row = 4
-        for i in self.pickList:       
-            a = Radiobutton(self, text = i.abr, variable = self.chosenRes, value = i)
-            a.grid(row = row, column = 0, sticky = W)
-            self.reslist.append(a)
-            row += 1
+            self.pickList.append(x)
         
-        self.submitButton = Button(self, text = "Next", command = lambda:[self.downloadVid, self.vidDetails])
-        self.submitButton.grid(row = row, column = 0, sticky=W)
+        self.submitButton = Button(self, text = "Next", command = self.downloadAud)
+        self.submitButton.grid(row = 4, column = 0, sticky=W)
         self.reslist.append(self.submitButton)
         
     def vidDetails(self):
         video_url = str(self.url.get())
-        youtube = YouTube(video_url)
-        urllib.request.urlretrieve(youtube.thumbnail_url, "thumb.png")
+        self.youtube = YouTube(video_url)
+        urllib.request.urlretrieve(self.youtube.thumbnail_url, "thumb.png")
         img = ImageTk.PhotoImage(Image.open("thumb.png").resize((160, 90)))
-        Label(self, text = youtube.title).grid(row = 1, column = 4, sticky=W)
+        Label(self, text = self.youtube.title).grid(row = 1, column = 4, sticky=W)
 
         l= Label(self, image = img)
         l.grid(row=0,column=4)
@@ -93,7 +97,9 @@ class Application(Frame):
         self.vidDetails()
         self.chooseResVid()
 
-        
+    def runAudFunc(self):
+        self.vidDetails()
+        self.chooseResAudio()
         
     def create_widgets(self):
         Label(self, text = "YouTube Downloader", justify="center", font=("Helvetica", 20, "bold")).grid(row = 0, column = 0, columnspan = 2)
@@ -110,10 +116,16 @@ class Application(Frame):
         Button(self, text="Browse", command = self.browse).grid(row = 2, column = 2)
         self.vidButton = Button(self, text = "Download video", command = self.runVidFunc)
         self.vidButton.grid(row = 3, column = 0, sticky=W)
-        self.audButton = Button(self, text = "Download audio", command = self.chooseResAudio)
+        self.audButton = Button(self, text = "Download audio", command = self.runAudFunc)
         self.audButton.grid(row = 3, column = 1, sticky=W)
+
+def on_closing():
+    if os.path.exists("thumb.png"):
+        os.remove("thumb.png")
+    root.destroy()
 
 root = Tk()
 root.title("YouTube Downloader")
+root.protocol("WM_DELETE_WINDOW", on_closing)
 app = Application(root)
 root.mainloop()
